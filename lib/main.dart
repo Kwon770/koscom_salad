a46f4a9d55ef082160a3e85b6b32dae2769d196a
Kwon770 <sckwon770@gmail.com>
2025-03-11 10:30:56 +0900

    feat: Add supabase and dotenv for supabase secret

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:koscom_salad/screens/home_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  // 환경변수 로드
+  await dotenv.load(fileName: ".env");
+
+  // Supabase 초기화
+  final supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  final supabaseKey = dotenv.env['SUPABASE_KEY']!;
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+
   runApp(const MyApp());
 }
+
+// Supabase 클라이언트 인스턴스
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
